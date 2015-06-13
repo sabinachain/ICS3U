04014098bf31bf210145085a9446b8ae6b2587ce
sbeleuz <sbeleuz@bayviewglen.ca>
2015-06-13 16:47:37 -0400

    Revert "Revert "Added critical items. Must add characters. Since they are not being""
    This reverts commit 9aad47e3281ea9308bf6f8db82ddebd0dff34127.

--- Zork/src/com/bayviewglen/zork/Parser.java
+++ Zork/src/com/bayviewglen/zork/Parser.java
@@ -73,7 +73,7 @@ class Parser implements Serializable
         // note: we just ignore the rest of the input line.
 
         // Now check whether this word is known. If so, create a command
-        // with it. If not, create a "nil" command (for unknown command).
+        // with it. If not, create a "null" command (for unknown command).
 
         if(commands.isCommand(word1))
             return new Command(word1, word2, word3, word4);
